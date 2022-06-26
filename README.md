19846beb7d030fdbade69f41dbbb88609b6eabbd
camille.chomel <camille.chomel@FOXE-FR>
2022-06-26 18:34:57 +0200

    Working

---
+++ README.md
@@ -0,0 +1,9 @@
+# JSON Beautifier
+
+Takes either a file or stdin, and outputs a pretty-printed JSON to the standard output.
+
+## Usage
+
+> json-beautifier -f ./file.json
+
+> cat ./file.json | json-beautifier
--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/alexflint/go-arg"
+	"io/ioutil"
+	"os"
+)
+
+var args struct {
+	JSONFilePath string `arg:"-f" help:"path to the JSON file"`
+}
 
 func main() {
-	fmt.Println("Hello World !")
+	arg.MustParse(&args)
+
+	var rawContent []byte
+
+	if args.JSONFilePath != "" {
+		content, err := ioutil.ReadFile(args.JSONFilePath)
+		if err != nil {
+			panic(fmt.Errorf("error when reading the JSON file: %w", err))
+		}
+
+		rawContent = content
+	} else {
+		reader := bufio.NewReader(os.Stdin)
+		var err error
+		var input byte
+		for {
+			input, err = reader.ReadByte()
+			if err != nil {
+				break
+			} else {
+				rawContent = append(rawContent, input)
+			}
+		}
+	}
+
+	var output any
+	err := json.Unmarshal(rawContent, &output)
+	if err != nil {
+		panic(fmt.Errorf("error when unmarshalling JSON content: %w", err))
+	}
+
+	formattedOutput, err := json.MarshalIndent(output, "", " ")
+
+	fmt.Println(string(formattedOutput))
 }
